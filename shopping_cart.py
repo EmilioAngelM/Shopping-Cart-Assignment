a527c93f3d8f0b46b4bf4408ac35180805739a47
EmilioAngelM <77894543+EmilioAngelM@users.noreply.github.com>
2021-02-23 00:53:25 -0500

    Appending loop

--- shopping_cart.py
+++ shopping_cart.py
@@ -45,6 +45,7 @@ def to_usd(my_price):
 #Info Inputs
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ") #>string version of numbers
@@ -52,12 +53,20 @@ while True:
     if selected_id == "DONE":
         break
     else:
+        #matching_product = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_product[0]
+        #
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
+#print(selected_ids)
+
+#print("TOTAL PRICE: " + str(total_price))
+
+for selected_id in selected_ids:
         matching_product = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_product[0]
         
         total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-
-
 print("TOTAL PRICE: " + str(total_price))
